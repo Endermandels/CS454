8d45294f4a45ea73223a358de740ba73e66a5500
Endermandels <elijahdelavar@gmail.com>
2024-08-22 16:07:02 -0700

    Get HTML pages within narrow domain

--- web_crawler.py
+++ web_crawler.py
@@ -8,20 +8,24 @@ x/xx/2024
 import requests						# requests webpages
 from bs4 import BeautifulSoup		# converts html to english
 from urllib.robotparser import RobotFileParser
-									# parses robots.txt
-import re							# regular expressions
-import time
+from urllib.parse import urlparse, urljoin
 from copy import deepcopy
+import time 
+import re
 
 DEBUG = True
-SEED_URLS = [
-	('https://en.wikipedia.org/wiki/Main_Page', 
-		'https://en.wikipedia.org/robots.txt')
-] # (url, robots.txt)
+USER_AGENT = '*'
 
 def dprint(s):
 	if DEBUG:
 		print(s)
+
+
+def filter_links(href):
+	"""
+	Filter invalid or unhelpful links
+	"""
+	return href
 
 def main():
 	"""
@@ -42,10 +46,70 @@ def main():
 	"""
 
 	# Queue
-	qq = deepcopy(SEED_URLS)
+	qq = [
+		'https://en.wikipedia.org/wiki/Main_Page', 
+	]
+	robots_urls = [
+		'https://en.wikipedia.org/robots.txt'
+	]
+	domains = [
+		urlparse(qq[0]).netloc
+	]
 
-	# rp = RobotFileParser()
-	# rp.set_url()
+	# Metadata
+	touched = set()
+	pages_visited = 0
+	current_domain = 0
+	count = 0 # For setting a specified limit
+
+	rp = RobotFileParser()
+	rp.set_url(robots_urls[0])
+	rp.read()
+
+	while len(qq) > 0:
+		try:
+			url = qq.pop(0)
+
+			# Do not open restricted URLs
+			if not rp.can_fetch(USER_AGENT, url):
+				dprint(f'Skipping URL: {url}')
+				continue
+
+			link = requests.get(url)
+			# Current_url may be different from url
+			#   in case the random page is accessed
+			current_url = link.url 
+			touched.add(current_url)
+
+			# Check if response is HTML
+			if not 'text/html' in link.headers.get('Content-Type'):
+				dprint(f'Skipping non-HTML content: {current_url}')
+				continue
+
+			# SAVE THIS!!!
+			page = BeautifulSoup(link.text, 'html.parser')
+			
+			# Parse for new links (BFS)
+			body = page.find(id='bodyContent')
+			for link in body.find_all('a', href=filter_links):
+				full_url = urljoin(current_url, link['href'])
+				link_domain = urlparse(full_url).netloc
+				if (full_url in touched or 
+					link_domain != domains[current_domain]):
+					continue
+				else:
+					qq.append(full_url)
+					touched.add(full_url)
+
+			# DEBUG Metadata
+			pages_visited += 1
+			dprint(f'Length of Queue: {len(qq)}')
+			dprint(f'Pages visited: {pages_visited}')
+
+			time.sleep(1)
+		except Exception as e:
+			print(f'Encountered error: {e}')
+			print('Exiting...')
 
 if __name__ == '__main__':
 	main()
