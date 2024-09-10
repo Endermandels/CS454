c5bac02bb468657e1940781f87108e857e62d462
Endermandels <elijahdelavar@gmail.com>
2024-09-10 08:56:13 -0700

    Store term counts, Compute TF

--- Assignment 2/tf_idf.py
+++ Assignment 2/tf_idf.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import math
 import csv
 
@@ -5,15 +6,27 @@ class TF_IDF(object):
 	def __init__(self, dataFile):
 		# Docs: key = doc id, value = doc contents
 		self.docs = self.load_data(dataFile)
-		print(self.docs)
 
 	def load_data(self, dataFile):
+		"""
+		Each document id is the key in docs
+		The value in docs is a tuple containing the total number of terms and
+			the count of each term in the document
+		"""
 		docs = dict()
 
 		with open(dataFile, mode='r') as file:
 			csvFile = csv.reader(file)
+			DocStats = namedtuple('DocStats', ['size', 'counts'])
 			for row in csvFile:
-				docs[row[0]] = row[1]
+				term_counts = dict()
+				terms = row[1].split()
+				for term in terms:
+					if not term in term_counts:
+						term_counts[term] = 1
+					else:
+						term_counts[term] += 1
+				docs[row[0]] = DocStats(len(terms), term_counts)
 
 		return docs
 
@@ -24,11 +37,11 @@ class TF_IDF(object):
 		pass
 
 	def tf(self, d, t):
-		pass
-		# return math.log(1 + )
+		return math.log(1 + self.docs[d].counts[t] / self.docs[d].size)
 
 def main():
 	tf_idf = TF_IDF('wine.csv')
+	print(tf_idf.tf('4999', 'fruit'))
 
 if __name__ == '__main__':
 	main()
