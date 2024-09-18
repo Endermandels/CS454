5056c17bc4f2cd84920db6cb37dd8a84c42c96b3
Endermandels <elijahdelavar@gmail.com>
2024-09-18 12:54:27 -0700

    Add assignment 3 files

---
+++ Assignment 3/assignment3.py
@@ -0,0 +1,19 @@
+import math
+
+class Ranking(object):
+    """
+    Rank query satisfaction
+    """
+    
+    def __init__(self, judgement_file):
+        pass
+    def prec(self, query_line, thresh):
+        pass
+    def recall(self, query_line, thresh):
+        pass
+    def rr(self, query_line, thresh):
+        pass
+    def f1_score(self, query_line, thresh):
+        pass
+    def ndcg(self, query_line):
+        pass
