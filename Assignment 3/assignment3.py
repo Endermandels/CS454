4f2b645b09256eecf170fd6dabb93c8dab019761
Endermandels <elijahdelavar@gmail.com>
2024-09-18 14:07:38 -0700

    Calculate precision

--- Assignment 3/assignment3.py
+++ Assignment 3/assignment3.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import math
 
 class Ranking(object):
@@ -6,14 +7,48 @@ class Ranking(object):
     """
     
     def __init__(self, judgement_file):
-        pass
+        """
+        url_scores = {
+            query: {
+                url: score
+            }
+        }
+        """
+        
+        self.NUM_RETURNED = 10
+        self.url_scores = defaultdict(dict)
+        with open(judgement_file, 'r') as file:
+            for line in file:
+                parts = line.strip().split('\t')
+                self.url_scores[parts[0]][parts[1]] = int(parts[2])
+                
     def prec(self, query_line, thresh):
-        return -1
+        """
+        Returns precision
+        
+        Formula: rel / k
+        terms:
+            rel - relevance of a url based on judgement score
+            k   - number of returned results
+        """
+        parts = query_line.split()
+        rel = 0
+        
+        for url in parts[3:self.NUM_RETURNED+3]:
+            score = self.url_scores[parts[0]].get(url, -1)
+            if score >= thresh:
+                rel += 1
+        
+        return rel / self.NUM_RETURNED
+
     def recall(self, query_line, thresh):
         return -1
+
     def rr(self, query_line, thresh):
         return -1
+
     def f1_score(self, query_line, thresh):
         return -1
+
     def ndcg(self, query_line):
         return -1
