e0e14abbd360c04ab82353bfc8a637c937a339b5
Endermandels <elijahdelavar@gmail.com>
2024-09-18 13:29:16 -0700

    Make while loop for testing file

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -3,15 +3,50 @@ import sys
 
 def main(judgement_file, query_results):
     ranking = Ranking(judgement_file)
+    lines = []
     
     with open(query_results, 'r') as file:
-        first_line = file.readline()
-        prec = ranking.prec(first_line.strip(), 0)
-        print(prec)
-        
-        # for line in file:
-        #     prec = ranking.prec(line.strip(), 0)
-        #     print(prec)
+        lines = file.read().replace('\t', ' ').split('\n')
+
+    try:
+        while True:
+            test_input = input('Enter <method> <line_number> <threshold>\n').split(' ')
+            
+            if len(test_input) != 3:
+                print('Incorrect number of arguments\n')
+                continue
+            
+            method = test_input[0]
+            line_num = int(test_input[1])
+            threshold = int(test_input[2])
+            
+            if line_num >= len(lines):
+                print('Line number does not exist\n')
+                continue
+            if not (0 <= threshold <= 4):
+                print('Threshold does not exist\n')
+                continue
+            
+            satisfaction = -1
+            
+            if method == 'prec':
+                satisfaction = ranking.prec(lines[line_num].strip(), threshold)
+            elif method == 'recall':
+                satisfaction = ranking.recall(lines[line_num].strip(), threshold)
+            elif method == 'rr':
+                satisfaction = ranking.rr(lines[line_num].strip(), threshold)
+            elif method == 'f1':
+                satisfaction = ranking.f1_score(lines[line_num].strip(), threshold)
+            elif method == 'ndcg':
+                satisfaction = ranking.ndcg(lines[line_num].strip())
+            else:
+                print('Method does not exist\n')
+                continue
+                
+            print('Satisfaction score:', satisfaction, '\n')
+                
+    except KeyboardInterrupt:
+        print('\nExiting...')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
