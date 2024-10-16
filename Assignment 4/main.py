4e3fd26ef93e36c9c5ee5318d290dda240b8ca35
Endermandels <elijahdelavar@gmail.com>
2024-10-15 17:51:08 -0700

    Start implementing index over docs

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -1,26 +1,61 @@
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.qparser import QueryParser
+from bs4 import BeautifulSoup
 from whoosh import index
 import os.path
 import shutil
+import pickle
+import sys
 
 IND_PATH = '_index'
+DOCS_PATH = 'docs'
+URL_MAP_PATH = '_url_map.dat'
 
 def create_index(fn):
-    if (os.path.exists(fn)):
+    if os.path.exists(fn):
         shutil.rmtree(fn)
 
-
     os.makedirs(fn)
-    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
+    schema = Schema(url=ID(stored=True, unique=True), title=TEXT(stored=True), content=TEXT(stored=True))
     return index.create_in(fn, schema)
 
-def add_docs(ind):
-    writer = ind.writer()
-    writer.add_document(title=u'doc', content=u'py doc hello big world oh', path=u'/a')
-    writer.add_document(title=u'doc2', content=u'my oh my', path=u'/b')
-    writer.commit()
+def unpickle_url_map(fn):
+    """
+    @return url_map (key = url, val = doc path)
+    """
 
+    try:
+        with open(fn, 'rb') as file:
+            return pickle.load(file)
+    except Exception as e:
+        print(f'! Error while parsing {fn}: {e}')
+        sys.exit(1)
+
+def extract_content(html):
+    page = BeautifulSoup(html, 'html.parser')
+    title = page.title.string if page.title else 'No Title'
+    content = page.get_text().lower()
+    return title, content
+
+def add_docs(ind, docs_fn, url_map):
+    if not os.path.exists(docs_fn):
+        print(f'! Error: No {docs_fn} folder')
+        sys.exit(1)
+
+    writer = ind.writer()
+    cnt = 1
+    batch_size = 2
+
+    for url, fn in url_map.items():
+        if os.path.isfile(fn):
+            with open(fn, 'r', encoding='utf-8') as file:
+                title, content = extract_content(file.read())
+                writer.add_document(url=url, title=title, content=content)
+                if cnt % batch_size == 0:
+                    writer.commit()
+                    writer = ind.writer()
+    
+    writer.commit()
 
 def query_session(ind):
     try:
@@ -40,7 +75,8 @@ def query_session(ind):
 
 def main():
     ind = create_index(IND_PATH)
-    add_docs(ind)
+    url_map = unpickle_url_map(URL_MAP_PATH)
+    add_docs(ind, DOCS_PATH, url_map)
     query_session(ind)
 
 if __name__ == '__main__':
